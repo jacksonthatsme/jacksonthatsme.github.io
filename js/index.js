2fdfac47d555654d98103422ce455abbf181856a
Jackson Lynch <jacksonthatsme@gmail.com>
2017-01-26 03:28:07 -0500

    updated portfolio, added ga script and favicon

--- js/index.js
+++ js/index.js
@@ -1,8 +1,10 @@
 $(document).ready(function(){
   var playVideo = $('[data-js="play-video"]');
+  var scrollDown = $('[data-js="scroll-down"]');
+  var scrollToAbout = $('[data-js="scroll-to-about"]');
   
   
-  playVideo.on('click', function(){
+  playVideo.on('click', function() {
     var parentMedia = this.closest('[data-js="media-object"]');
     var videoEl = $(parentMedia).find('video')[0];
     videoEl.load();
@@ -16,5 +18,19 @@ $(document).ready(function(){
       buttonBack.text('Play');
       mediaOverlay.removeClass('hide');
     }, false);
+  });
+  
+  scrollDown.on('click', function() {
+    var firstProject = $('section.project').first();
+    $('html,body').animate({
+      scrollTop: firstProject.offset().top
+    }, 600);
+  });
+  scrollToAbout.on('click', function(e) {
+    e.stopPropagation();
+    var aboutSection = $('section#about');
+    $('html,body').animate({
+      scrollTop: aboutSection.offset().top
+    }, 900);
   });
 });
